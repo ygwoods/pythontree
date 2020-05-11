480b97c7c492a54be6b643b7178cd1dd4987195f
ygwoods <ygwoods@gmail.com>
2020-05-12 01:07:23 +0800

    note

--- demo.py
+++ demo.py
@@ -13,35 +13,27 @@ class Tool(Tk):
         self.dic={}
         self.dictv={}
         self.diclist={}
-        self.style=Style()
-        self.style.configure("F.TButton", relief='flat',font=('tahoma','16','normal'),foreground="black", background="white")
-        self.style.configure("L.Treeview", borderwidth=1,relief='flat',foreground="black", background="white")
         self.title('树图笔记')
+        print(t.root.node)
         self.geometry("%sx%s+%s+%s"%t.root.pos)
         with open("tmp.ico","wb+") as f:
             f.write(base64.b64decode(icon))
         self.iconbitmap("tmp.ico")
         os.remove("tmp.ico")
         self.textboard=None
-        self.top=None
-        self.tbflag=False
+        self.listview=None 
+        self.top = None
 
         #define view windget
 
         self.treeview=Treeview(self,height=27,show="tree");self.treeview.heading('#0',text='树目')
-        self.treeview.column('#0',width=250)
         self.searchview=Entry(self)
-        self.listview=Treeview(self,height=26,show='headings',style='L.Treeview',columns=('a','b','c','d'))
-        for (ia,iw,it) in [('a',30,'no.'),('b',200,'tree'),('c',200,'tag'),('d',150,'link')]:
-            self.listview.column(ia,width=iw)
-            self.listview.heading(ia,text=it)
+        ft=Font(family='等线',size=12)
+        self.textboard=Text(self,bg='snow',fg='maroon',font=ft)
         self.tbar=Scrollbar(self.treeview,orient='horizontal')
-        self.sbar=Scrollbar(self.listview)
-        self.listview.config(yscrollcommand=self.sbar.set)
         self.treeview.config(xscrollcommand=self.tbar.set)
         self.tbar.config(command=self.treeview.xview)
-        self.sbar.config(command=self.listview.yview)
-
+        
         #add menu
         menubar = Menu(self)
         self.fMenu=Menu(menubar,tearoff=0)
@@ -50,7 +42,6 @@ class Tool(Tk):
         menubar.add_cascade(label = "File ",menu = self.fMenu)
         menubar.add_cascade(label =  "Edit ",menu = self.eMenu)
         menubar.add_cascade(label = "Set  ",menu = self.sMenu)
-        menubar.add_command(label = "About",command = self.about)
         #self.fMenu.add_command(label='open', command=None)
         self.fMenu.add_command(label='Save', command=t.dump)
         self.fMenu.add_command(label='Save to Server', command=lambda :showinfo(message=t.dumptoweb()))
@@ -58,20 +49,19 @@ class Tool(Tk):
         self.eMenu.add_command(label='Copy  Tree', command=None)
         self.eMenu.add_command(label='Paste Tree', command=None)
         self.eMenu.add_command(label='Del   Tree', command=self.delete)
-        self.sMenu.add_command(label='On/off textboard', command=self.showtextboard)
-        self.sMenu.add_command(label='Check for Updates', command=lambda:self.openweb("http://www.yuke.wang"))
-        self.sMenu.add_command(label='Current version：0.1', command=None)
+        self.sMenu.add_command(label='Check for Updates', command=lambda:webbrowser.open("http://www.yuke.wang/"))
         self.config(menu=menubar)
 
         #layout view
+        
         self.tbar.pack(side='bottom',fill='x',expand=1,anchor="s")
         self.treeview.pack(side='left',fill='y',ipadx=50,anchor="w")
-        #Label(self,width=0).pack(side='left',fill='y',ipadx=0,ipady=0,padx=0,pady=0,anchor="w")
         self.searchview.pack(side='top',fill='both',anchor="w")
-        self.listview.pack(side='top',fill='both',ipadx=2,expand=1,anchor="w")
-        self.sbar.pack(side='right',fill='y',anchor="e")
-        self.viewbind()
+        self.textboard.pack(side='top',fill='both',expand=1,anchor="w")
+
+
         #init data
+        #self.top.deiconify()
         for i in t.root.child:
             tv=self.treeview.insert('','end',text=i.node)
             self.dic[i]=tv
@@ -79,70 +69,44 @@ class Tool(Tk):
             self.travel(i.child)
         for i in range(0,len(t.querylist)):
             self.listview.insert('','end',text=t.querylist[i].node)
+        self.viewbind()
     def viewbind(self):           #bind event handle function
         self.bind('<Configure>',self.tbset)
         self.bind('<space>',lambda e: self.searchview.focus_force())
         self.bind('<Escape>',lambda e: self.home(e))
-        self.bind('<quoteleft>',lambda e: self.home(e))
         self.treeview.bind('<<TreeviewSelect>>',self.showtree)
-        self.listview.bind('<<TreeviewSelect>>',self.treeshow)
-        for i in ['0','1','2','3','4','5','6','7','8','9']:self.bind("<KeyPress-%s>"%i,self.treeishow)
-        #self.treeview.bind('<2>',self.copy_tree)
+        self.textboard.bind('<Control-KeyPress-s>',self.addtreetext)
         for i in ['<Return>','<Delete>','<Up>']:self.searchview.bind(i,self.action)
-        self.searchview.bind('<Shift-KeyPress-7>',self.seturl)
-    def seturl(self,e):
-        t.root.url=self.searchview.get()
+        
     def saveto(self):
         t.dumptoweb()
-    def openweb(self,urls):
-        webbrowser.open(urls)
-    def showtextboard(self):
-        if not self.top:
-            self.top=Toplevel()
-            x=t.root.pos
-            self.top.geometry("%sx%s+%s+%s"%(str(int(self.winfo_screenwidth())-int(x[0])-int(x[2])),x[1],str(int(x[0])+int(x[2])+10),x[3]))
-            self.top.overrideredirect(1)
-            self.top.attributes('-alpha', 0.95)
-            ft=Font(family='等线',size=12)
-            self.textboard=Text(self.top,bg='snow',fg='maroon',font=ft)
-            self.textboard.bind('<Control-KeyPress-s>',self.addtreetext)
-            self.textboard.pack(fill='both',expand=1)
-            self.tbflag=True
-            return
-        if self.tbflag :
-            self.top.withdraw()
-            self.tbflag=False
-        else:
-            self.top.deiconify()
-            self.tbflag=True
+
     def tbset(self,e):
         if e.widget==self and int(e.width)>300:
             t.root.pos=(e.width,str(int(e.height)+25),e.x,e.y)
-        if self.tbflag:
-            x=t.root.pos
-            self.top.geometry("%sx%s+%s+%s"%(str(int(self.winfo_screenwidth())-int(x[0])-int(x[2])),x[1],str(int(x[0])+int(x[2])+10),x[3]))
-    def about(self):
-        pass
+        if self.top:
+            self.top.deiconify()
+            #x=t.root.pos
+            #self.top.geometry("%sx%s+%s+%s"%(str(int(self.winfo_screenwidth())-int(x[0])-int(x[2])),x[1],str(int(x[0])+int(x[2])+10),x[3]))
+
     def addtreetext(self,e):
         text=self.textboard.get('1.0','end')
         t.activetree.text=text
     def treeishow(self,e):
         i=int(e.keysym)
         if i<=len(t.querylist) and len(t.querylist):self.treeview.see(self.dic[t.querylist[i]])
+        if(self.top):
+            self.top.deiconify()
     def treeshow(self,e):
         tr=self.diclist[self.listview.selection()[0]]
         self.treeview.see(self.dic[tr])
     def showtree(self,e):
         if  not self.treeview.selection() :return
         t.activetree=self.dictv[self.treeview.selection()[0]]
-        for item in self.listview.get_children():self.listview.delete(item)
-        if self.tbflag:
-            self.textboard.delete('1.0','end')
-            if hasattr(t.activetree, 'text'):
-                self.textboard.insert('end',t.activetree.text)
-        for i in t.activetree.child:
-            self.listview.insert('','end',i.node)
-        #listview show child
+        #for item in self.listview.get_children():self.listview.delete(item)
+        self.textboard.delete('1.0','end')
+        if hasattr(t.activetree, 'text'):
+            self.textboard.insert('end',t.activetree.text)
     def delete(self):
         if t.activetree==self.dictv[self.treeview.selection()[0]]:
             tv=self.dic[t.activetree]
@@ -158,6 +122,7 @@ class Tool(Tk):
             self.dictv[tv]=tree
             self.travel(tree.child)
     def home(self,e):
+        t.root.url='https://yuke.wang/note/'
         self.treeview.selection_set(())
         t.activetree=t.root
     def action(self,e):#searchview events
@@ -166,7 +131,23 @@ class Tool(Tk):
         if e.keysym=='Return':
             t.query(entr_str)
             lv=''
-            for item in self.listview.get_children():self.listview.delete(item)
+            self.top=Toplevel()
+            self.top.geometry("300x400+50+50")
+            #self.top.overrideredirect(1)
+            self.top.attributes('-alpha', 0.95)
+            #self.top.withdraw()
+            self.listview=Treeview(self.top,height=26,show='headings',style='L.Treeview',columns=('a','b','c','d'))
+            for (ia,iw,it) in [('a',30,'no.'),('b',200,'tree'),('c',200,'tag'),('d',150,'link')]:
+                self.listview.column(ia,width=iw)
+                self.listview.heading(ia,text=it)
+            self.sbar=Scrollbar(self.listview)
+            self.listview.config(yscrollcommand=self.sbar.set)
+            self.sbar.config(command=self.listview.yview)
+            self.listview.bind('<<TreeviewSelect>>',self.treeshow)
+            for i in ['0','1','2','3','4','5','6','7','8','9']:self.listview.bind("<KeyPress-%s>"%i,self.treeishow)
+            self.listview.pack(side='top',fill='both',ipadx=2,expand=1,anchor="w")
+            self.sbar.pack(side='right',fill='y',anchor="e")
+            #for item in self.listview.get_children():self.listview.delete(item)
             for i in range(0,len(t.querylist)):
                 tg=lk=''
                 if hasattr(t.querylist[i], 'tag'):tg=t.querylist[i].tag
@@ -178,7 +159,6 @@ class Tool(Tk):
 
         elif e.keysym=='Up':
             if len(entr_str)>20:return
-            #tr=t.Tree()
             tr=t.add(entr_str)
             if not tr.parent.parent:
                 self.dic[tr]=self.treeview.insert('','end',text=tr.node)
--- tool.py
+++ tool.py
@@ -3,7 +3,6 @@ import requests
 import os.path,time
 from tree import *
 querylist=[]
-
 def load():
     fr =requests.get(root.url+'/data')
     if fr.status_code==200:
@@ -17,15 +16,26 @@ def dump():
     if isinstance(root,Tree):
         with open('data','wb+') as f:
             p.dump(root,f)
+        with open('root.txt','w+') as f:
+            t.root.layer=0
+            stk=[t.root] 
+            while stk:
+                tr = stk.pop()
+                tr.layer=tr.parent.layer+1
+                f.write(tr.layer+'/t'+tr.node+'/t'+tr.text+'/n')
+                if tr.child:
+                    stk.extend(tr.child)
+
 def dumptoweb():
     if isinstance(root,Tree):
         with open('data','wb+') as f:
             p.dump(root,f)
         with open('data','rb') as f:
             files={'a':f}
-            t=requests.post(root.url+'/upload.php',files=files)
+            t=requests.post(root.url+'upload.php',files=files)
             if 200 == t.status_code:return 'ok'
             else:return 'network error:'+t
+
 
 def initdata():
     if os.path.isfile('data'):
@@ -34,7 +44,7 @@ def initdata():
     else:
         tr=Tree('root')
         tr.pos=('600','550','300','0')
-        tr.url='http://localhost'
+        tr.url='https://yuke.wang/note'
         return tr
 activetree=root=initdata()    
 def add(node):
@@ -87,42 +97,3 @@ def layer():
         a=a+1
         pin = pin.parent
     return a
-def router(str):
-    global activetree,root
-    if len(str)<=2:#pick a tree
-        try:
-            i=int(str)
-            if i<=len(querylist):
-                activetree=querylist[i]
-                a=layer()
-                print("L%d"%a,activetree.node)
-            else:print('out of range')
-        except ValueError:
-            if str[0]=='t':
-                travel([activetree])
-            elif str[0]=='d':
-                delete()
-            elif str[0]=='s':
-                dump()
-            elif str[0]=='n':
-                root=Tree('root')
-                dump()
-            elif str[0]=='x':
-                a=layer()
-                print("L%d"%a,activetree.node)
-            elif str[0]=='<':activetree=activetree.parent
-            else:pass
-    else:
-        if str[0]=='a':
-            add(str[2:])
-        elif str[0]=='q':
-            query(str[2:])
-        elif str[0]=='u':
-            update(str[2:])
-        else:
-            print('help message s- save q- query a-add  t- travel d- delete u- update n- newtree')  
-if __name__ == "__main__":
-    a=''
-    while a !='!q':
-        a=input()
-        router(a)
